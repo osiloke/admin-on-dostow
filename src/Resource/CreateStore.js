af023a464bb2c06591c97e3ea3e694a5e90756b9
Osiloke Emoekpere <osi@progwebtech.com>
2017-10-05 20:29:10 +0100

    enhancements

--- src/Resource/CreateStore.js
+++ src/Resource/CreateStore.js
@@ -1,31 +1,48 @@
 import React from 'react'
-import { Create, SimpleForm, SelectInput, ReferenceInput, BooleanInput, NumberInput, TextInput, DateInput, LongTextInput } from 'admin-on-rest';
+import {
+    Create,
+    SimpleForm,
+    SelectInput,
+    ReferenceInput,
+    BooleanInput,
+    NumberInput,
+    TextInput,
+    DateInput,
+    LongTextInput,
+} from 'admin-on-rest'
 import { getDisplayField, validateInputForm } from '../Lib/Resource'
 import ramda from 'ramda'
 
 export default ({ store }) => {
     const fields = store.properties ? Object.keys(store.properties) : []
-    return (props) => (
-        <Create {...props }>
+    return props => (
+        <Create {...props} title={store.title}>
             <SimpleForm validate={validateInputForm(store)}>
-                {fields.map((field) => {
+                {fields.map(field => {
                     const elem = store.properties[field]
                     const params = {
                         label: elem.title,
                         source: field,
-                        key: `${elem.id}-${field}`
+                        key: `${elem.id}-${field}`,
                     }
                     if (elem.type === 'array')
-                        return <TextInput
-                            {...params}
-                            format={v => v !== undefined ? ramda.join(',', v) : v}
-                            parse={v => v !== undefined ? ramda.split(',', v) : v} />
+                        return (
+                            <TextInput
+                                {...params}
+                                format={v =>
+                                    v !== undefined ? ramda.join(',', v) : v}
+                                parse={v =>
+                                    v !== undefined ? ramda.split(',', v) : v}
+                            />
+                        )
                     if (elem.ref && elem.ref.target !== '_static') {
-                        return null 
+                        return null
                     }
                     if (typeof elem.type === 'string') {
-                        if (elem.type === 'date-time'
-                            || elem.type === 'datetime')
+                        if (
+                            elem.type === 'date-time' ||
+                            elem.type === 'datetime'
+                        )
                             return <DateInput {...params} />
                         if (elem.type === 'string') {
                             if (elem.format === 'long')
@@ -34,13 +51,16 @@ export default ({ store }) => {
                         }
                         if (elem.type === 'boolean')
                             return <BooleanInput {...params} />
-                        if (elem.type === 'number'
-                            || elem.type === 'float'
-                            || elem.type === 'integer')
+                        if (
+                            elem.type === 'number' ||
+                            elem.type === 'float' ||
+                            elem.type === 'integer'
+                        )
                             return <NumberInput {...params} />
                     }
                     return <TextInput key={params.key} {...params} />
                 })}
             </SimpleForm>
-        </Create>)
+        </Create>
+    )
 }
--- src/Resource/ListStore.js
+++ src/Resource/ListStore.js
@@ -1,48 +1,78 @@
-import React from 'react';
+import React from 'react'
 import {
-    List, Datagrid, TextField, TextInput,
-    Filter, ReferenceInput, SelectInput, NumberField,
-    BooleanField, DateField,
-    FunctionField, EditButton
+    List,
+    Datagrid,
+    TextField,
+    TextInput,
+    Filter,
+    ReferenceInput,
+    SelectInput,
+    NumberField,
+    BooleanField,
+    DateField,
+    FunctionField,
+    EditButton,
 } from 'admin-on-rest'
 import { getDisplayField } from '../Lib/Resource'
 import PropTypes from 'prop-types'
 
-const makeFilters = (storeDef) => (props) => {
-    const fields = storeDef.required && storeDef.required.length > 0 ? [
-        <TextInput key="search" label="Search" source="q" alwaysOn />, ...storeDef.required.map((field) => (storeDef.properties[field]
-            && storeDef.properties[field].ref) ?
-            (<ReferenceInput
-                key={`${storeDef.name}${field}`}
-                label={field}
-                source={field}
-                reference={storeDef.properties[field].ref.target}
-                allowEmpty>
-                <SelectInput optionText={getDisplayField} />
-            </ReferenceInput>) : <TextInput key={`${storeDef.name}${field}`} source={field} />
-        )] : [<TextInput key="search" label="Search" source="q" alwaysOn />]
-    return (
-        <Filter {...props}>
-            {fields}
-        </Filter>
-    )
-};
+const makeFilters = storeDef => props => {
+    const fields =
+        storeDef.required && storeDef.required.length > 0
+            ? [
+                  <TextInput key="search" label="Search" source="q" alwaysOn />,
+                  ...storeDef.required.map(
+                      field =>
+                          storeDef.properties[field] &&
+                          storeDef.properties[field].ref ? (
+                              <ReferenceInput
+                                  key={`${storeDef.name}${field}`}
+                                  label={field}
+                                  source={field}
+                                  reference={
+                                      storeDef.properties[field].ref.target
+                                  }
+                                  allowEmpty
+                              >
+                                  <SelectInput optionText={getDisplayField} />
+                              </ReferenceInput>
+                          ) : (
+                              <TextInput
+                                  key={`${storeDef.name}${field}`}
+                                  source={field}
+                              />
+                          )
+                  ),
+              ]
+            : [<TextInput key="search" label="Search" source="q" alwaysOn />]
+    return <Filter {...props}>{fields}</Filter>
+}
 
 const ListStore = ({ store }) => {
-    const fields = store.properties ? Object.keys(store.properties) : []
+    const hidden = store.hidden || []
+    const fields = store.properties ? Object.keys(store.properties).filter( w => hidden.indexOf(w) == -1) : []
     const Filters = makeFilters(store)
-    return (props) => (
-        <List {...props}  title={store.title} perPage={25} filters={<Filters store={store} />}>
+    return props => (
+        <List
+            {...props}
+            title={store.title}
+            perPage={25}
+            filters={<Filters store={store} />}
+        >
             <Datagrid>
                 <FunctionField
                     key={`${store.name}-id`}
-                    label='id'
-                    render={(record) => record.id.substring(0, 8)} />
-                {(fields.length > 7 ? fields.slice(0, 7) : fields).map((field) => {
+                    label="id"
+                    render={record => record.id.substring(0, 8)}
+                />
+                {(fields.length > 7
+                    ? fields.slice(0, 7)
+                    : fields
+                )..map(field => {
                     const elem = store.properties[field]
                     const params = {
                         key: `${store.id}-${field}`,
-                        source: field
+                        source: field,
                     }
                     if (elem.ref && elem.ref.target !== '_static') {
                         return <TextField {...params} source={field} />
@@ -56,17 +86,25 @@ const ListStore = ({ store }) => {
                         }
                         if (elem.type === 'boolean')
                             return <BooleanField {...params} source={field} />
-                        if (elem.type === 'number'
-                            || elem.type === 'float'
-                            || elem.type === 'integer')
+                        if (
+                            elem.type === 'number' ||
+                            elem.type === 'float' ||
+                            elem.type === 'integer'
+                        )
                             return <NumberField {...params} source={field} />
                     }
-                    return <TextField key={params.key} {...params} source={field} />
+                    return (
+                        <TextField
+                            key={params.key}
+                            {...params}
+                            source={field}
+                        />
+                    )
                 })}
                 <EditButton basePath="/posts" />
             </Datagrid>
         </List>
-    );
+    )
 }
 
 export default ListStore
